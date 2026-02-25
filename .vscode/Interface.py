c77232e64698b139072e93bf315251b5b6789479
kemwel86 <graceayengyese14@gmail.com>
2026-02-25 11:09:07 +0100

    Update Interface.py

--- .vscode/Interface.py
+++ .vscode/Interface.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 app=tk.Tk()
-app.title("GESTION DE MEDICAMENT POUR PHARMACIE")
+app.title("GESTION DES STOCKS DESPRODUITSPHARMACEUTIQUE")
 app.geometry("800x500")
 app.configure(bg="lightgray")
 
